e860d9e1feab02dece7353b5d5b75bf7471752cb
Abhilash Majumder <30946547+abhilash1910@users.noreply.github.com>
2024-11-06 16:48:30 +0800

    [SYCLomatic]Enable migration for CUBLASLT_MATMUL_DESC_BIAS_DATA_TYPE & BIAS_POINTER (#2385)

--- clang/lib/DPCT/MapNames.cpp
+++ clang/lib/DPCT/MapNames.cpp
@@ -2005,6 +2005,12 @@ void MapNames::setExplicitNamespaceMap(
       {"CUBLASLT_MATMUL_DESC_SCALE_TYPE",
        getLibraryHelperNamespace() +
            "blas_gemm::experimental::matmul_desc_t::attribute::scale_type"},
+      {"CUBLASLT_MATMUL_DESC_BIAS_DATA_TYPE",
+       getLibraryHelperNamespace() +
+           "blas_gemm::experimental::matmul_desc_t::attribute::bias_type"},
+      {"CUBLASLT_MATMUL_DESC_BIAS_POINTER",
+       getLibraryHelperNamespace() +
+           "blas_gemm::experimental::matmul_desc_t::attribute::bias_pointer"},
       {"CUBLASLT_MATMUL_DESC_POINTER_MODE",
        getLibraryHelperNamespace() +
            "blas_gemm::experimental::matmul_desc_t::attribute::pointer_mode"},
